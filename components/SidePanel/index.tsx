38a344bf9632d7c056192be9fcadfbe3dbd23f0a
oleksandr.hnennyi <oleksandr.hnennyi@mev.com>
2022-04-10 23:32:01 +0300

    added admin tabs without functionality

--- components/SidePanel/index.tsx
+++ components/SidePanel/index.tsx
@@ -4,29 +4,41 @@ import styles from './SidePanel.module.scss';
 export const SidePanel = () => {
   const router = useRouter();
 
-  // console.log(router.pathname.endsWith(''))
-
   return (
     <div className={styles.container}>
-      <div className={styles.buttonContainer}>
-        <div className={styles.active}></div>
-        <button>
-          <img src={'../assets/images/instagram.png'} />
-          <p>Витрати</p>
-        </button>
+      <div style={{ flex: 1 }}>
+        <div className={styles.buttonContainer}>
+          <div
+            className={`${styles.active}` + ' ' + `${router.pathname.endsWith('expenses') && styles.activeVisible}`}
+          ></div>
+          <button onClick={() => router.push('/panel/expenses')}>
+            <img src={'../assets/images/file-minus.svg'} />
+            <p>Витрати</p>
+          </button>
+        </div>
+        <div className={styles.buttonContainer}>
+          <div
+            className={`${styles.active}` + ' ' + `${router.pathname.endsWith('incomes') && styles.activeVisible}`}
+          ></div>
+          <button onClick={() => router.push('/panel/incomes')}>
+            <img src={'../assets/images/file-plus.svg'} />
+            <p>Внески</p>
+          </button>
+        </div>
+        <div className={styles.buttonContainer}>
+          <div
+            className={`${styles.active}` + ' ' + `${router.pathname.endsWith('accounts') && styles.activeVisible}`}
+          ></div>
+          <button onClick={() => router.push('/panel/accounts')}>
+            <img src={'../assets/images/credit-card.svg'} />
+            <p>Рахунки</p>
+          </button>
+        </div>
       </div>
-      <div className={styles.buttonContainer}>
-        <div className={styles.active}></div>
-        <button className={styles.sideButton}>
-          <img src={'../assets/images/file-minus.svg'} />
-          <p>Внески</p>
-        </button>
-      </div>
-      <div className={styles.buttonContainer}>
-        <div className={styles.active}></div>
-        <button>
-          <img src={'../assets/images/credit-card.svg'} />
-          <p>Рахунки</p>
+      <div className={styles.buttonContainer + ' ' + styles.buttonBottom}>
+        <button onClick={() => router.push('/logout')}> {/* TODO: Logout page */}
+          <img src={'../assets/images/exit.svg'} />
+          <p>Вихід</p>
         </button>
       </div>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,23 +8,10 @@ import { SidePanel } from '../components/SidePanel';
 const store = setupStore();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  //IF AUTHED REDIRECT TO THE ADMIN PANEL?
-
-  // url === 'panel' {
-  //   if(authorized) {
-  //     leftNav
-  //     <Component {...pageProps} />
-  //   } else {
-  //     redirect
-  //   }
-  // }
   const router = useRouter();
 
   if (router.pathname.includes('panel')) {
     if (true) { // TODO: Add validation for authentication
-      // if (router.pathname.endsWith('panel')) {
-      //   router.
-      // }
       return (
         <>
           <SidePanel />
