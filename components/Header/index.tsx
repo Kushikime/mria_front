792c25b1e126b3ce100507a097c850f626696277
KushiNii <eric.demchak@waweoc.com>
2022-04-16 22:23:59 +0200

    added dohuja chogo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
 
 interface IHeaderProps {
@@ -7,9 +8,36 @@ interface IHeaderProps {
 
 
 
+const content = {
+    "en": {
+        menu: {
+            whoWeAre: 'About us',
+            whatWeDo: 'What we do',
+            team: 'Team',
+            partners: 'Partners',
+            contacts: 'Contact'
+        },
+        helpBtn: 'Support'
+    },
+    "uk": {
+      menu: {
+          whoWeAre: 'ХТО МИ',
+          whatWeDo: 'що робимо',
+          team: 'КОМАНДА',
+          partners: 'пАРТНЕРИ',
+          contacts: 'Контакти'
+      },
+      helpBtn: 'Допомогти'
+    },
+  };
 
 
 const Header = (props: IHeaderProps) => {
+    const { locale, locales, defaultLocale } = useRouter();
+
+    //@ts-ignore
+    const { menu, helpBtn } = content[locale];
+
     return (
         <div className={styles.header}>
             <div className={styles.wrapper}>
@@ -20,24 +48,29 @@ const Header = (props: IHeaderProps) => {
 
                     <div className={styles.content}>
                         <ul>
-                            <li><a href="#">ХТО МИ</a></li>
-                            <li><a href="#">що робимо</a></li>
-                            <li><a href="#">КОМАНДА</a></li>
-                            <li><a href="#">ЗВІТНІСТЬ</a></li>
-                            <li><a href="#">пАРТНЕРИ</a></li>
-                            <li><a href="#">Контакти</a></li>
+                            <li>
+                                <a href="#">{menu.whoWeAre}</a>
+                            </li>
+                            <li><a href="#">{menu.whatWeDo}</a></li>
+                            <li><a href="#">{menu.team}</a></li>
+                            {/* <li><a href="#">ЗВІТНІСТЬ</a></li> */}
+                            <li><a href="#">{menu.partners}</a></li>
+                            <li><a href="#">{menu.contacts}</a></li>
                         </ul>
                     </div>
                 </div>
                 <div className={styles.right}>
                     <div className={styles.language_drop}>
-                        <img src="/assets/images/ua_flag.png" alt="" />    
+                        
                         <p>UA</p>
                         <span></span>
                     </div>
-
                     <div className={styles.btn}>
-                        <button><span>Допомогти</span></button>
+                        <div className={styles.gradientBorder}>
+                            {/* <button></button> */}
+                            <span>{helpBtn}</span>
+                            <img src="/assets/images/ua_flag.png" alt="" />    
+                        </div>
                     </div>
                 </div>
             </div>
