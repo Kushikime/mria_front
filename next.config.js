0409a88d5a41b95a3058f179745965dc983c2b53
KushiNii <eric.demchak@waweoc.com>
2022-05-05 17:35:05 +0200

    changed socket url route

--- next.config.js
+++ next.config.js
@@ -9,5 +9,8 @@ module.exports = {
     locales: ['uk', 'en'],
     defaultLocale: 'uk'
   },
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://api.domrii.com.ua',
+  },
   reactStrictMode: false
 }
